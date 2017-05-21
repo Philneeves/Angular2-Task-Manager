e8b756ad472f1fbe4844e35f5221ab16992384b4
Philneeves <p.neeves@gmail.com>
2017-05-21 22:28:09 +0100

    added firebase to prodn environ

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,11 @@
 export const environment = {
-  production: true
+  production: true,
+  firebase: {
+  	apiKey: "AIzaSyACOtHWEnov-HhbigVcO4NAemDNgEyrS2I",
+    authDomain: "phils-task-manager.firebaseapp.com",
+    databaseURL: "https://phils-task-manager.firebaseio.com",
+    projectId: "phils-task-manager",
+    storageBucket: "phils-task-manager.appspot.com",
+    messagingSenderId: "438888457167"
+  }
 };
